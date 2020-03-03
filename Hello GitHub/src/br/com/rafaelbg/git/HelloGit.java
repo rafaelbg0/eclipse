869a32fb54de484341d448a32e8eb57df6006301
rafaelbg0 <57422768+rafaelbg0@users.noreply.github.com>
2020-03-03 19:44:56 -0300

    Adicionei arquivos via upload

---
+++ Hello GitHub/src/br/com/rafaelbg/git/HelloGit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.rafaelbg.git;
+
+/**
+ * @author r.barbosa.goncalves
+ * Exemplo de integração do Eclipse com o GitHub
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+
+	}
+
+}
